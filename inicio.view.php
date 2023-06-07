0f4d01b8a66f27cb8768e8bea6221b85da29aa30
Geni2002 <Geni2002@gmail.com>
2023-06-07 12:06:47 -0600

    se agrego proyecto

---
+++ inicio.view.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<?php
+require 'functions.php';
+$permisos = ['Administrador','Profesor','Padre'];
+permisos($permisos);
+
+?>
+<html>
+<head>
+<title>Inicio | Registro de Notas</title>
+    <meta name="description" content="Registro de Notas del Centro Escolar Profesor Lennin" />
+    <link rel="stylesheet" href="css/style.css" />
+
+</head>
+<body>
+<div class="header">
+        <h1>Registro de Notas </h1>
+        <h3>Usuario:  <?php echo $_SESSION["username"] ?></h3>
+</div>
+<nav>
+    <ul>
+        <li class="active"><a href="inicio.view.php">Inicio</a> </li>
+        <li><a href="alumnos.view.php">Registro de Alumnos</a> </li>
+        <li><a href="listadoalumnos.view.php">Listado de Alumnos</a> </li>
+        <li><a href="notas.view.php">Registro de Notas</a> </li>
+        <li><a href="listadonotas.view.php">Consulta de Notas</a> </li>
+        <li><a href="condicionsalud.php">Condicion de Salud</a> </li>
+        <li class="right"><a href="logout.php">Salir</a> </li>
+
+    </ul>
+</nav>
+
+<div class="body">
+    <div class="panel">
+           <h1 class="text-center">Secundaria Tecnica # 10.</h1>
+           <img src="" class="rounded" alt="">
+        <?php
+        if(isset($_GET['err'])){
+            echo '<h3 class="error text-center">ERROR: Usuario no autorizado</h3>';
+        }
+       
+        ?>
+        <br>
+        <hr>
+        <p class="text-center"><strong></strong><br><br><br><br></p>
+        <br>
+        </div>
+</div>
+
+<footer>
+
+    <p>Derechos reservados &copy; 2020</p>
+</footer>
+
+</body>
+
+</html>
